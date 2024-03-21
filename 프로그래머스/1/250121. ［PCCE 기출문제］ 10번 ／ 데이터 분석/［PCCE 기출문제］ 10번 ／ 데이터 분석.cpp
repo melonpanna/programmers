883935844dc0618a0e7a7beb5887bdb0c24e8c6d
Melonpanna <87775722+melonpanna@users.noreply.github.com>
2024-03-21 21:35:39 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.48 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+map<string,int> m;
+bool cmp(vector<int> v1,vector<int> v2,int k){
+    return v1[k]<v2[k];
+}
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    vector<vector<int>> answer;
+    m["code"]=0;
+    m["date"]=1;
+    m["maximum"]=2;
+    m["remain"]=3;
+    //추출-java에서는 stream으로 가능하려나
+    for(int i=0;i<data.size();i++){
+        if(data[i][m[ext]]<val_ext)answer.push_back(data[i]);
+    }
+    sort(answer.begin(), answer.end(), [&](const auto& a, const auto& b) {
+        return cmp(a, b, m[sort_by]); // 오름차순으로 정렬
+    });
+    return answer;
+}
