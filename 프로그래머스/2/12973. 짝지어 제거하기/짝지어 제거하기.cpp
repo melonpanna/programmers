aa5b6b7cb3e01b42409f948934da7f6c842ab12d
Melonpanna <87775722+melonpanna@users.noreply.github.com>
2024-05-23 20:45:54 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.40 ms, Memory: 6.57 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(st.empty())st.push(s[i]);
+        else{
+            if(st.top()==s[i])st.pop();
+            else st.push(s[i]);
+        }
+    }
+    if(st.empty())return 1;
+    return 0;
+}
