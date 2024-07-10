f4ad415997fa0510ac11873eb88cb933c4e8b3e9
Melonpanna <87775722+melonpanna@users.noreply.github.com>
2024-07-10 16:22:56 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,4 @@
 SELECT food_type, rest_id, rest_name, favorites
 from rest_info r
-having favorites = (select max(favorites) from rest_info group by food_type having food_type = r.food_type)
+where favorites = (select max(favorites) from rest_info group by food_type having food_type = r.food_type)
 order by food_type desc;
